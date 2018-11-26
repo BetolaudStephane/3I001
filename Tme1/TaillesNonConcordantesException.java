3b94056910fbf58efa41b31d6486a70d6b3f914a
BetolaudStephane <45333962+BetolaudStephane@users.noreply.github.com>
2018-11-26 16:10:02 +0100

    Create TaillesNonConcordantesException.java

---
+++ Tme1/TaillesNonConcordantesException.java
@@ -0,0 +1,13 @@
+public class TaillesNonConcordantesException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public TaillesNonConcordantesException() {
+		System.out.println("La taille des matrices ne concorde pas");
+	}
+
+	
+}
