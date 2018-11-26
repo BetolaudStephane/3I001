23a15d10cbfb52e489f662a18557627ca3c98f22
BetolaudStephane <45333962+BetolaudStephane@users.noreply.github.com>
2018-11-26 16:14:35 +0100

    Create TaillesNonConcordantesException.java

---
+++ Tme3/TaillesNonConcordantesException.java
@@ -0,0 +1,17 @@
+package Exo3;
+
+
+
+public class TaillesNonConcordantesException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public TaillesNonConcordantesException() {
+		System.out.println("La taille des matrices ne concorde pas");
+	}
+
+	
+}
